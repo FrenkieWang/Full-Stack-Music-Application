fac64e1975b3c5ddc5176fbda7a186dd285fdbbd
Frenkie Wang <frenkiewang21@gmail.com>
2025-03-14 13:08:11 +0000

    Change HOST_URL

--- frontend/src/components/Album.js
+++ frontend/src/components/Album.js
@@ -4,7 +4,7 @@ import { Link } from "react-router-dom";
 import styled from "styled-components";
 import SongsModal from "./SongsModal";
 
-const HOST_URL = 'http://localhost:5000';
+const HOST_URL = process.env.REACT_APP_HOST_URL || 'http://localhost:5000';
 const ALBUM_API_URL = `${HOST_URL}/albums`; 
 const ARTIST_API_URL = `${HOST_URL}/artists`;
 
